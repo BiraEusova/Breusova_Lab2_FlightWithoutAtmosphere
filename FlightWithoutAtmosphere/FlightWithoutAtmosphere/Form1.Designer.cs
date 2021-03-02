0b26f8e9777cb2b997789da115122a01c5c58dd5
Ka3achka <ira.breusova@gmail.com>
2021-03-02 21:16:10 +0700

    Second lab is done

---
+++ FlightWithoutAtmosphere/FlightWithoutAtmosphere/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace FlightWithoutAtmosphere
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.heightNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.speedNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.angelNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.speedLabel = new System.Windows.Forms.Label();
+            this.angelLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.outputTimeLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.angelNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.outputTimeLabel);
+            this.panel1.Controls.Add(this.timeLabel);
+            this.panel1.Controls.Add(this.stopButton);
+            this.panel1.Controls.Add(this.startButton);
+            this.panel1.Controls.Add(this.heightNumericUpDown);
+            this.panel1.Controls.Add(this.speedNumericUpDown);
+            this.panel1.Controls.Add(this.angelNumericUpDown);
+            this.panel1.Controls.Add(this.heightLabel);
+            this.panel1.Controls.Add(this.speedLabel);
+            this.panel1.Controls.Add(this.angelLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // heightNumericUpDown
+            // 
+            this.heightNumericUpDown.Location = new System.Drawing.Point(16, 166);
+            this.heightNumericUpDown.Name = "heightNumericUpDown";
+            this.heightNumericUpDown.Size = new System.Drawing.Size(120, 26);
+            this.heightNumericUpDown.TabIndex = 5;
+            // 
+            // speedNumericUpDown
+            // 
+            this.speedNumericUpDown.Location = new System.Drawing.Point(16, 101);
+            this.speedNumericUpDown.Maximum = new decimal(new int[] {
+            300000000,
+            0,
+            0,
+            0});
+            this.speedNumericUpDown.Name = "speedNumericUpDown";
+            this.speedNumericUpDown.Size = new System.Drawing.Size(120, 26);
+            this.speedNumericUpDown.TabIndex = 4;
+            this.speedNumericUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // angelNumericUpDown
+            // 
+            this.angelNumericUpDown.Location = new System.Drawing.Point(16, 43);
+            this.angelNumericUpDown.Maximum = new decimal(new int[] {
+            89,
+            0,
+            0,
+            0});
+            this.angelNumericUpDown.Name = "angelNumericUpDown";
+            this.angelNumericUpDown.Size = new System.Drawing.Size(120, 26);
+            this.angelNumericUpDown.TabIndex = 3;
+            this.angelNumericUpDown.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.heightLabel.Location = new System.Drawing.Point(12, 143);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(62, 20);
+            this.heightLabel.TabIndex = 2;
+            this.heightLabel.Text = "Height";
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.speedLabel.Location = new System.Drawing.Point(12, 78);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(61, 20);
+            this.speedLabel.TabIndex = 1;
+            this.speedLabel.Text = "Speed";
+            // 
+            // angelLabel
+            // 
+            this.angelLabel.AutoSize = true;
+            this.angelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.angelLabel.Location = new System.Drawing.Point(12, 20);
+            this.angelLabel.Name = "angelLabel";
+            this.angelLabel.Size = new System.Drawing.Size(55, 20);
+            this.angelLabel.TabIndex = 0;
+            this.angelLabel.Text = "Angel";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(16, 263);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(120, 53);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(172, 0);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(628, 450);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.Location = new System.Drawing.Point(16, 322);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(120, 52);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timeLabel.Location = new System.Drawing.Point(12, 218);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(52, 20);
+            this.timeLabel.TabIndex = 8;
+            this.timeLabel.Text = "Time:";
+            // 
+            // outputTimeLabel
+            // 
+            this.outputTimeLabel.AutoSize = true;
+            this.outputTimeLabel.Location = new System.Drawing.Point(61, 218);
+            this.outputTimeLabel.Name = "outputTimeLabel";
+            this.outputTimeLabel.Size = new System.Drawing.Size(0, 20);
+            this.outputTimeLabel.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.angelNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.Label speedLabel;
+        private System.Windows.Forms.Label angelLabel;
+        private System.Windows.Forms.NumericUpDown heightNumericUpDown;
+        private System.Windows.Forms.NumericUpDown speedNumericUpDown;
+        private System.Windows.Forms.NumericUpDown angelNumericUpDown;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label outputTimeLabel;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ FlightWithoutAtmosphere/FlightWithoutAtmosphere/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlightWithoutAtmosphere
+{
+    public partial class Form1 : Form
+    {
+        const double dt = 0.01;
+        const double g = 9.81;
+        bool pause = false;
+
+        double a;
+        double v0, v0x, v0y;
+        double y0;
+        double t;
+        double x, y;
+        double outputT;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!pause)
+            {
+                a = (double)angelNumericUpDown.Value * Math.PI / 180;
+                v0 = (double)speedNumericUpDown.Value;
+                y0 = (double)heightNumericUpDown.Value;
+
+                v0x = v0 * Math.Cos(a);
+                v0y = v0 * Math.Sin(a);
+
+                t = 0;
+                outputT = 0;
+                x = 0;
+                y = y0;
+
+                double xAdd = v0x * (Math.Sqrt(v0y * v0y + 2 * g * y0) - v0y) / g;
+                // Если тело брошено с какой-то высоты y0, то обычная формула расчета 
+                // максимальной дальности полета не подойдет (данная формула основана
+                // на времени подъема тела до самой высокой точки), т.к. при падении относительно самой высокой точки полета
+                // тело по оси оХ преодолевает большее расстояние, чем до этой точки.
+                // Следовательно, надо прибавить расстояние, 
+                // пройденное телом по оси оХ после достижения уровня высоты у0 при падении.
+                // Если y0 = 0, то xAdd также = 0.
+
+
+                double xMax = v0 * v0 * Math.Sin(2 * a) / g + xAdd;
+                double yMax = y0 + v0 * v0 * Math.Sin(a) * Math.Sin(a) / (2 * g);
+
+                chart1.ChartAreas[0].AxisX.Maximum = xMax + 0.5;
+                chart1.ChartAreas[0].AxisY.Maximum = yMax + 0.5;
+
+                chart1.Series[0].Points.Clear();
+                chart1.Series[0].Points.AddXY(x, y);
+                outputTimeLabel.Text = t.ToString() + " sec";
+            }
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pause = false;
+            t += dt;
+            x = v0x * t;
+            y = y0 + v0y * t - g * t * t / 2;
+            chart1.Series[0].Points.AddXY(x, y);
+
+            outputT += Math.Round(t, 3);
+            outputTimeLabel.Text = outputT.ToString() + " sec";
+
+            if (y <= 0) timer1.Stop();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            pause = true;
+            timer1.Stop();
+        }
+    }
+}
